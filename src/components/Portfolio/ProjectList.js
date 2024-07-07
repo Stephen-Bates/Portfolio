6c1f0f958be96d7d6f364d0b1d741e6c6b040f31
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-07-07 11:23:04 -0400

    added component for Portfolio and Project

---
+++ src/components/Portfolio/ProjectList.js
@@ -0,0 +1,47 @@
+import bookSearchSnapshot from '/src/assets/book-search-snapshot.png'
+import noteTakerSnapshot from '/src/assets/Note-Taker-snapshot.png'
+import passwordGeneratorSnapshot from '/src/assets/Password-Generator-snapshot.png'
+import sojournalSnapshot from '/src/assets/soJournal-snapshot.png'
+import textAppSnapshot from '/src/assets/text-app-snapshot.png'
+import weatherDashSnapshot from '/src/assets/Weather-Dash-snapshot.png'
+
+const ProjectList = [
+    {
+        title: 'SoJournal',
+        description: 'A digital diary for keeping track of',
+        img: sojournalSnapshot,
+        address: 'https://github.com/Stephen-Bates/soJournal'
+    },
+    {
+        title: 'JATE',
+        description: 'A PWA text editor',
+        img: textAppSnapshot,
+        address: 'https://github.com/Stephen-Bates/Text-App'
+    },
+    {
+        title: 'Password Generator',
+        description: 'A simple password generator applicatoin',
+        img: passwordGeneratorSnapshot,
+        address: 'https://github.com/Stephen-Bates/Password-Generator'
+    },
+    {
+        title: 'Note Taker',
+        description: 'A simple note taking application',
+        img: noteTakerSnapshot,
+        address: 'https://github.com/Stephen-Bates/Note-Taker'
+    },
+    {
+        title: 'Book Search',
+        description: 'This app utilizes a MERN stack to allow users to register with the site and save a list of books that they can search for',
+        img: bookSearchSnapshot,
+        address: 'https://github.com/Stephen-Bates/Book-Search'
+    },
+    {
+        title: 'Weather Dash',
+        description: 'This project was a daily+5 day weather forecast dashboard',
+        img: weatherDashSnapshot,
+        address: 'https://github.com/Stephen-Bates/Weather-Dash'
+    },
+]
+
+export default ProjectList
